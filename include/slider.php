1e26e80af6a995451c4f5b291028c96f8bd3464b
kienhoang255 <hoangtrungkien255@gmail.com>
2021-12-19 15:40:03 +0700

    Update User slider.php

--- include/slider.php
+++ include/slider.php
@@ -1,37 +1,77 @@
 <!-- banner -->
-	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-		<!-- Indicators-->
-		<ol class="carousel-indicators">
-			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-			<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-			<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-		</ol>
-		<div class="carousel-inner">
-			<?php
-			$sql_slider = mysqli_query($con,"SELECT * FROM tbl_slider WHERE slider_active='1' ORDER BY slider_id");
-			while($row_slider = mysqli_fetch_array($sql_slider)){ 
-			?>
-			<div class="carousel-item item1 active">
-				<div class="container">
-					<div class="w3l-space-banner">
-						<div class="carousel-caption p-lg-5 p-sm-4 p-3">
-							<p><?php echo $row_slider['slider_caption'] ?></p>
-						</div>
-					</div>
-				</div>
-			</div>
-			<?php
-			} 
-			?>
-		</div>
-		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-			<span class="sr-only">Previous</span>
-		</a>
-		<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-			<span class="carousel-control-next-icon" aria-hidden="true"></span>
-			<span class="sr-only">Next</span>
-		</a>
-	</div>
-	<!-- //banner -->
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <!-- Indicators-->
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item item1 active">
+            <div class="container">
+                <div class="w3l-space-banner">
+                    <div class="carousel-caption p-lg-5 p-sm-4 p-3">
+                        <p>Nhận ngay
+                            <span>10%</span> khi thanh toán bằng VNPay</p>
+                        <h3 class="font-weight-bold pt-2 pb-lg-5 pb-4">Tất cả mặt hàng
+                            <span>Giảm Giá Cực Khủng</span>
+                        </h3>
+                        <a class="button2" href="product.html">Mua ngay </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item item2">
+            <div class="container">
+                <div class="w3l-space-banner">
+                    <div class="carousel-caption p-lg-5 p-sm-4 p-3">
+                        <p>Tai nghe
+                            <span>KHÔNG DÂY</span> hãng Apple</p>
+                        <h3 class="font-weight-bold pt-2 pb-lg-5 pb-4">
+                            <span>Tai nghe</span>Tốt nhất mọi thời đại
+                        </h3>
+                        <a class="button2" href="product.html">Mua ngay </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item item3">
+            <div class="container">
+                <div class="w3l-space-banner">
+                    <div class="carousel-caption p-lg-5 p-sm-4 p-3">
+                        <p>Deal cực khủng
+                            <span>Giảm 10%</span> thanh toán bằng VNPay</p>
+                        <h3 class="font-weight-bold pt-2 pb-lg-5 pb-4">
+                            <span>Hãy thanh toán liền tay</span>
+                        </h3>
+                        <a class="button2" href="product.html">Mua ngay </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item item4">
+            <div class="container">
+                <div class="w3l-space-banner">
+                    <div class="carousel-caption p-lg-5 p-sm-4 p-3">
+                        <p>Mua ngay
+                            <span>40%</span> giảm giá</p>
+                        <h3 class="font-weight-bold pt-2 pb-lg-5 pb-4">Ngay tại thời điểm này
+                            <span>Giảm giá</span>
+                        </h3>
+                        <a class="button2" href="product.html">Mua ngay </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+<!-- //banner -->
